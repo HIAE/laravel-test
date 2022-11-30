961e888a8ffda579ac5b91d44f648e71de3f60fa
Rafael Lotto <ralomach@gmail.com>
2022-11-30 09:56:30 -0300

    Create LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (! Auth::attempt($credentials)) {
+            return response([
+                    'message' => 'As credenciais fornecidas não correspondem aos nossos registros',
+                ], 401)
+                ->header('Content-Type', 'text/plain');
+        }
+
+        $request->user()->tokens()->delete();
+
+        $token = $request->user()->createToken('api_token');
+
+        return ['token' => $token->plainTextToken];
+    }
+}
