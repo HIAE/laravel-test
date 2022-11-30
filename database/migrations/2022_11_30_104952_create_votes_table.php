5a093f896d059f2cbab514f7fbf39987ecdff3a2
klisostom <klis@pm.me>
2022-11-30 19:37:55 -0300

    feat(api): Novas migrations

---
+++ database/migrations/2022_11_30_104952_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->enum('note', [1,2,3,4,5]);
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('idea_id')->constrained('ideas');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+};
