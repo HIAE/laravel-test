8f2b1cff3ba2303072093b38fe8a969b08aa5cff
Rafael Lotto <ralomach@gmail.com>
2022-11-30 18:49:02 -0300

    Fix hash error on user's factory. Thanks Adenilton

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'password' => 'password',
             'is_admin' => false,
             'remember_token' => Str::random(10),
         ];
