b68fcfe024964ec8f0275b0fc713094109b152eb
klisostom <klis@pm.me>
2022-12-01 07:42:32 -0300

    test(api): Adicionando testes no group

--- tests/Feature/Group/GroupTest.php
+++ tests/Feature/Group/GroupTest.php
@@ -29,3 +29,49 @@ test('create group', function () {
         "description" => $data["description"],
     ]);
 });
+
+test('update group', function () {
+    // Arrange
+    $group = GroupModel::factory()->create();
+    $data = ['description' => 'new_user'];
+
+    // Act
+    $response = $this->putJson("/api/v1/groups/$group->uuid", $data);
+
+    // Assert
+    $response->assertOk();
+    $this->assertDatabaseHas('groups', [
+        "uuid" => $group->uuid,
+        "description" => $data["description"],
+    ]);
+});
+
+test('show group', function () {
+    // Arrange
+    $group = GroupModel::factory()->create();
+    $this->postJson(route('group.store', $group->toArray()));
+
+    // Act
+    $response = $this->getJson(route('group.show', $group->uuid));
+
+    // Assert
+    expect(JSON_ENCODE($response->json()))
+        ->json()
+        ->id->toBe($group->id)
+        ->uuid->toBe($group->uuid)
+        ->description->toBe($group->description);
+});
+
+test('list groups', function () {
+    // Arrange
+    $group = GroupModel::factory()->count(3)->create();
+
+    // Act
+    $response = $this->getJson(route('group.index'));
+
+    // Assert
+    expect($response->json('data'))
+        ->toHaveCount($group->count());
+
+    expect($response->json('data'))->toMatchArray($group->toArray());
+});
