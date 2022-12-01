07a6f03ec2ac488e22d1fe4b690050135e63b293
Rafael Lotto <ralomach@gmail.com>
2022-12-01 17:07:07 -0300

    Create VotePolicy

---
+++ app/Policies/VotePolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Vote;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VotePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Vote $vote)
+    {
+        return $user->id === $vote->user_id;
+    }
+}
