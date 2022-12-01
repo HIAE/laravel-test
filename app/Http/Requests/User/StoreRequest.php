9603e0c54bebc074d66f9f87956ffc187ccd8980
klisostom <klis@pm.me>
2022-12-01 13:07:21 -0300

    feat(api): Novas validações

---
+++ app/Http/Requests/User/StoreRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'uuid' => 'required|uuid',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email:rfc',
+            'password' => ['sometimes', 'required', 'confirmed', Password::min(8)]
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'uuid' => 'uuid',
+            'name' => 'nome',
+            'email' => 'email',
+            'password' => 'senha',
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email:rfc',
+            'password' => ['sometimes', 'required', 'confirmed', Password::min(8)]
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => 'nome',
+            'email' => 'email',
+            'password' => 'senha',
+        ];
+    }
+}
