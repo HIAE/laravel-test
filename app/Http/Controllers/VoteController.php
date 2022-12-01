0558dce55d0f24d3fe135b604272a8099bdfbec0
Rafael Lotto <ralomach@gmail.com>
2022-12-01 17:07:26 -0300

    Implement Vote controller methods

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -2,7 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
+use App\Models\Vote;
+use Illuminate\Http\Request;
+
 class VoteController extends Controller
 {
-    //
+    public function store(Request $request, Idea $idea)
+    {
+        $alreadyVoted = $idea->votes()
+            ->where('user_id', '=', $request->user()->id)
+            ->exists();
+
+        if ($alreadyVoted) {
+            return response([
+                'message' => 'Seu voto já foi realizado para esta ideia'
+            ], 400);
+        }
+
+        $idea->votes()->create([
+            'user_id' => $request->user()->id,
+        ]);
+
+        return ['message' => 'Voto realizado com sucesso'];
+    }
+
+    public function destroy(Idea $idea, Vote $vote)
+    {
+        $this->authorize('delete', $vote);
+
+        $vote->delete();
+
+        return ['message' => 'Voto removido com sucesso'];
+    }
 }
