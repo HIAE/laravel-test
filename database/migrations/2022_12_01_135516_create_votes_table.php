b4b31332bccbe60b12ab565dec47c1d0c5f43195
Rafael Lotto <ralomach@gmail.com>
2022-12-01 11:32:26 -0300

    Create Vote model and migration

---
+++ database/migrations/2022_12_01_135516_create_votes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Idea::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['idea_id', 'user_id', 'deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+};
