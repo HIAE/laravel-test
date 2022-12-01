84aaccb15d52de5e46bd312f12d143e271b2f179
klisostom <klis@pm.me>
2022-12-01 19:50:53 -0300

    test(api): Novo teste

---
+++ tests/Feature/Category/CategoryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Models\Category as CategoryModel;
+
+test('model category has been soft deleted', function () {
+    // Arrange
+    $category = CategoryModel::factory()->create();
+
+    // Act
+    $response = $this->deleteJson(route('category.destroy', $category->uuid));
+
+    // Assert
+    $response->assertNoContent();
+    $this->assertSoftDeleted($category);
+});
+
+test('create category', function () {
+    // Arrange
+    $category = CategoryModel::factory()->make();
+    $data = $category->toArray();
+
+    // Act
+    $response = $this->postJson(route('category.store', $data));
+
+    // Assert
+    $response->assertCreated();
+    $this->assertDatabaseHas('categories', [
+        'uuid' => $data['uuid'],
+        'category' => $data['category'],
+    ]);
+});
+
+test('update category', function () {
+    // Arrange
+    $category = CategoryModel::factory()->create();
+    $data = ['category' => fake()->word];
+
+    // Act
+    $response = $this->putJson("/api/v1/categories/$category->uuid", $data);
+
+    // Assert
+    $response->assertOk();
+    $this->assertDatabaseHas('categories', [
+        "uuid" => $category->uuid,
+        "category" => $data["category"],
+    ]);
+});
+
+test('show category', function () {
+    // Arrange
+    $category = CategoryModel::factory()->create();
+
+    // Act
+    $response = $this->getJson(route('category.show', $category->uuid));
+
+    // Assert
+    expect($response->json('data'))
+        ->id->toBe($category->uuid)
+        ->category->toBe($category->category);
+});
+
+test('list category', function () {
+    // Arrange
+    $category = CategoryModel::factory()->count(3)->create();
+
+    // Act
+    $response = $this->getJson(route('category.index'));
+
+    // Assert
+    expect($response->json('data'))
+        ->toBeArray()
+        ->not->toBeEmpty()
+        ->toHaveCount($category->count());
+});
