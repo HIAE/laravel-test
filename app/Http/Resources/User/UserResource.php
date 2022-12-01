8fb858465ee19a2bd8aa2d1efc58f7ac94ffa019
klisostom <klis@pm.me>
2022-12-01 13:08:04 -0300

    feat(api): Novos resources

---
+++ app/Http/Resources/User/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use App\Http\Resources\GroupCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->uuid,
+            'name' => $this->name,
+            'email' => $this->email,
+            'groups' => (new GroupCollection($this->groups)),
+        ];
+    }
+}
