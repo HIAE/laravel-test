5fb6ef8a800f5ddd4a20cccd8574e8d154d0c213
klisostom <klis@pm.me>
2022-12-01 13:13:05 -0300

    feat(api): Renomeando, implementando controlador User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,62 +2,90 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\User\StoreRequest;
+use App\Http\Requests\User\UpdateRequest;
+use App\Http\Resources\User\UserCollection;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
+use App\Repositories\User\UserRepository;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     *  Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return new UserCollection(User::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  StoreRequest   $request
+     * @param  UserRepository $repository
+     * @return Response
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request, UserRepository $repository)
     {
-        //
+        $dataValidated = $request->validated();
+        $dataValidated['password'] = $dataValidated['password'] ?? Hash::make(Str::random(8));
+        $result = $repository->new($dataValidated);
+
+        return response()->json($result, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  User $user
+     * @return Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return (new UserResource($user))->toJson();
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  UpdateRequest  $request
+     * @param  User           $user
+     * @param  UserRepository $repository
+     * @return Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(
+        UpdateRequest $request,
+        User $user,
+        UserRepository $repository
+    ) {
+        $dataValidated = $request->validated();
+        $result = $repository->updateById($dataValidated, $user);
+
+        return $result ?
+            response()->json() :
+            response()->json(['error: Não foi possível atualizar os dados.'], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  User           $user
+     * @param  UserRepository $repository
+     * @return Response
      */
-    public function destroy($id)
-    {
-        //
+    public function destroy(
+        User $user,
+        UserRepository $repository,
+    ) {
+        $result = $repository->deleteById($user);
+        return $result ?
+            response()->json(status: 204) :
+            response()->json(['error: Não foi possível excluir os dados.'], 500);
     }
 }
