1f0c53be277043a4ba5849251287f2e0fc0ae32e
klisostom <klis@pm.me>
2022-12-01 16:27:09 -0300

    feat(api): Novas validações

---
+++ app/Http/Requests/Idea/StoreRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Idea;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'uuid' => 'required|uuid',
+            'idea' => 'required|string|max:255',
+            'user_id' => 'required|exists:users,uuid',
+            'category_id' => 'required|exists:categories,uuid',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'uuid' => 'uuid',
+            'idea' => 'idea',
+            'user_id' => 'id do usuário',
+            'category_id' => 'id da categoria',
+        ];
+    }
+}
