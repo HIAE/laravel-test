554cc13c6ee31eae5a4a839890a77836a71de958
klisostom <klis@pm.me>
2022-12-01 13:14:13 -0300

    feat(api): Adicionando campos e função no model

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'uuid',
     ];
 
     /**
@@ -47,10 +48,33 @@ class User extends Authenticatable
     ];
 
     /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    /**
      * Get the groups for the user.
      */
     public function groups()
     {
         return $this->belongsToMany(Group::class, 'user_group');
+    }
+
+    // Custom Methods
+
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param  mixed  $value
+     * @param  string|null  $field
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where('uuid', $value)->firstOrFail();
     }
 }
