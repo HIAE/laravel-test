ea5d7fc12ae9eb194cf0c5a6a6dea0b37c29a38f
Rafael Lotto <ralomach@gmail.com>
2022-12-02 10:56:04 -0300

    Add missing tests on IdeaTest

--- tests/Feature/IdeaTest.php
+++ tests/Feature/IdeaTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Idea;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
@@ -38,6 +39,31 @@ class IdeaTest extends TestCase
             ]);
     }
 
+    public function test_can_view_an_idea()
+    {
+        Sanctum::actingAs($this->user);
+
+        $idea = Idea::factory()->create();
+
+        $response = $this->getJson("/api/ideas/{$idea->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'title',
+                    'description',
+                    'user',
+                    'category',
+                    'status',
+                    'comments',
+                    'votes',
+                    'createdAt',
+                ],
+            ]);
+    }
+
     public function test_users_can_create_ideas()
     {
         Sanctum::actingAs($this->user);
@@ -65,5 +91,88 @@ class IdeaTest extends TestCase
                 'description' => $description,
                 'category_id' => $category->id,
             ]);
+    }
+
+    public function test_users_can_update_own_idea()
+    {
+        Sanctum::actingAs($this->user);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        $title = 'Novo título';
+        $description = 'Nova descrição';
+
+        $response = $this->putJson("/api/ideas/{$idea->id}", [
+            'title' => $title,
+            'description' => $description,
+            'categoryId' => 1,
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => 'Ideia atualizada com sucesso.',
+            ]);
+
+        $this->assertDatabaseHas('ideas', [
+            'title' => $title,
+            'description' => $description,
+            'category_id' => 1,
+        ]);
+    }
+
+    public function test_users_cannot_update_ideas_from_others()
+    {
+        Sanctum::actingAs($this->user);
+
+        $idea = Idea::factory()->create();
+
+        $response = $this->putJson("/api/ideas/{$idea->id}", [
+            'title' => '...',
+            'description' => '...',
+            'categoryId' => 1,
+        ]);
+
+        $response
+            ->assertStatus(403);
+    }
+
+    public function test_users_can_delete_own_idea()
+    {
+        Sanctum::actingAs($this->user);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        $response = $this->deleteJson("/api/ideas/{$idea->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => 'Ideia removida com sucesso.',
+            ]);
+
+        $idea->refresh();
+
+        $this
+            ->assertDatabaseHas('ideas', [
+                'id' => $idea->id,
+            ])
+            ->assertNotNull($idea->deleted_at);
+    }
+
+    public function test_users_cannot_delete_ideas_from_others()
+    {
+        Sanctum::actingAs($this->user);
+
+        $idea = Idea::factory()->create();
+
+        $response = $this->deleteJson("/api/ideas/{$idea->id}");
+
+        $response
+            ->assertStatus(403);
     }
 }
