9222ba92238ddb2476e1a8c7907602ea510f5253
Rafael Lotto <ralomach@gmail.com>
2022-12-01 22:53:54 -0300

    Create UserRequest

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\Cpf;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => [
+                'required',
+                'email',
+                'ends_with:@company.com',
+            ],
+            'cpf' => [
+                'required',
+                new Cpf,
+            ],
+            'password' => [
+                'required',
+                Password::min(8)->uncompromised(),
+            ],
+        ];
+    }
+}
