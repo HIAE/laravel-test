e9d11512679233edae5c34b4905b30216ba20e1d
klisostom <klis@pm.me>
2022-12-01 21:02:56 -0300

    feat(api): Nova factory
    De comment.

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Idea;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'uuid' => (string) Str::uuid(),
+            'commentary' => fake()->text,
+            'user_id' => User::factory(),
+            'idea_id' => Idea::factory(),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'deleted_at' => null,
+        ];
+    }
+}
