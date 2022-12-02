e832d1090152cdc021233dc71a65c2952c0da6e5
Rafael Lotto <ralomach@gmail.com>
2022-12-02 07:40:16 -0300

    Update UserService with all methods

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,14 +2,43 @@
 
 namespace App\Services;
 
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
 class UserService
 {
-    public function createApiToken()
+    public function createApiToken(User $user, $token_name = 'api_token')
     {
-        request()->user()->tokens()->delete();
+        $user->tokens()->delete();
 
-        $token = request()->user()->createToken('api_token');
+        return $user->createToken($token_name);
+    }
 
-        return $token;
+    public function createUser(UserRequest $request)
+    {
+        $user = User::create($request->validated());
+
+        return $this->createApiToken($user);
+    }
+
+    public function destroyUser(User $user): bool
+    {
+        return $user->delete();
+    }
+
+    public function getAllUsers()
+    {
+        return UserResource::collection(User::paginate());
+    }
+
+    public function showUser(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    public function updateUser(User $user, UserRequest $request): bool
+    {
+        return $user->update($request->validated());
     }
 }
