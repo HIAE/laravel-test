d3438aac7852525724832e5d4d4fd8425d7d818a
Rafael Lotto <ralomach@gmail.com>
2022-12-01 23:24:50 -0300

    Implement Status controller methods

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,7 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StatusRequest;
+use App\Models\Status;
+use App\Services\StatusService;
+
 class StatusController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(StatusService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @lrd:start
+     * Show all status
+     * @lrd:end
+     */
+    public function index()
+    {
+        return $this->service->getAllStatus();
+    }
+
+    /**
+     * @lrd:start
+     * Create a status
+     * @lrd:end
+     */
+    public function store(StatusRequest $request)
+    {
+        $this->authorize('create', Status::class);
+
+        $status = $this->service->createStatus($request);
+
+        return ['message' => "Status {$status->name} criado com sucesso"];
+    }
+
+    /**
+     * @lrd:start
+     * Update a status
+     * @lrd:end
+     */
+    public function update(StatusRequest $request, Status $status)
+    {
+        $this->authorize('update', $status);
+
+        $this->service->updateStatus($status, $request);
+
+        return ['message' => 'Status atualizado com sucesso'];
+    }
+
+    /**
+     * @lrd:start
+     * Delete a status
+     * @lrd:end
+     */
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+
+        $this->service->destroyStatus($status);
+
+        return ['message' => 'Status removido com sucesso'];
+    }
 }
