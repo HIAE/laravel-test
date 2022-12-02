831ff8b3a0851d0f21423b4835e8d127a28ef786
Rafael Lotto <ralomach@gmail.com>
2022-12-02 14:57:35 -0300

    Add missing tests on UserTest

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -10,6 +12,18 @@ class UserTest extends TestCase
     use RefreshDatabase;
 
     protected $seed = true;
+
+    private $user;
+
+    private $admin;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->admin = User::factory()->admin()->create();
+    }
 
     public function test_users_can_register()
     {
@@ -41,5 +55,173 @@ class UserTest extends TestCase
             ->assertJson([
                 'message' => 'O CPF deve ser válido.',
             ]);
+    }
+
+    public function test_admins_can_list_users()
+    {
+        Sanctum::actingAs($this->admin);
+
+        $response = $this->getJson('/api/users');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data',
+                'links',
+                'meta',
+            ]);
+    }
+
+    public function test_users_cannot_list_users()
+    {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->getJson('/api/users');
+
+        $response
+            ->assertStatus(403);
+    }
+
+    public function test_admins_can_show_users()
+    {
+        Sanctum::actingAs($this->admin);
+
+        $response = $this->getJson("/api/users/{$this->user->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'emailVerifiedAt',
+                    'cpf',
+                    'isAdmin',
+                    'createdAt',
+                    'updatedAt',
+                ],
+            ]);
+    }
+
+    public function test_users_cannot_show_other_users()
+    {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->getJson("/api/users/{$this->admin->id}");
+
+        $response
+            ->assertStatus(403);
+    }
+
+    public function test_admins_can_edit_users()
+    {
+        Sanctum::actingAs($this->admin);
+
+        $data = [
+            'name' => 'Novo nome',
+            'email' => 'email@company.com',
+        ];
+
+        $response = $this->putJson("/api/users/{$this->user->id}", $data);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => "O usuário #{$this->user->id} foi atualizado com sucesso."
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+        ]);
+    }
+
+    public function test_users_cannot_edit_other_users()
+    {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->putJson("/api/users/{$this->admin->id}", [
+            'name' => '...',
+            'email' => 'other@company.com',
+        ]);
+
+        $response
+            ->assertStatus(403);
+    }
+
+    public function test_users_can_edit_themselves()
+    {
+        Sanctum::actingAs($this->user);
+
+        $data = [
+            'name' => 'Outro nome',
+            'email' => 'outro@company.com',
+        ];
+
+        $response = $this->putJson("/api/users/{$this->user->id}", $data);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => "O usuário #{$this->user->id} foi atualizado com sucesso."
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+        ]);
+    }
+
+    public function test_admins_can_delete_users()
+    {
+        Sanctum::actingAs($this->admin);
+
+        $response = $this->deleteJson("/api/users/{$this->user->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => "O usuário #{$this->user->id} foi removido com sucesso."
+            ]);
+
+        $this->user->refresh();
+
+        $this
+            ->assertDatabaseHas('users', [
+                'id' => $this->user->id,
+            ])
+            ->assertNotNull($this->user->deleted_at);
+    }
+
+    public function test_users_cannot_delete_other_users()
+    {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->deleteJson("/api/users/{$this->admin->id}");
+
+        $response
+            ->assertStatus(403);
+    }
+
+    public function test_users_can_delete_themselves()
+    {
+        Sanctum::actingAs($this->user);
+
+        $response = $this->deleteJson("/api/users/{$this->user->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'message' => "O usuário #{$this->user->id} foi removido com sucesso."
+            ]);
+
+        $this->user->refresh();
+
+        $this
+            ->assertDatabaseHas('users', [
+                'id' => $this->user->id,
+            ])
+            ->assertNotNull($this->user->deleted_at);
     }
 }
